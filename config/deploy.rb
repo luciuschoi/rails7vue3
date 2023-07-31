8907374db632071848c3af812ae756009b62cd05
Lucius Choi <lucius.choi@gmail.com>
2023-08-01 07:25:37 +0900

    IP 주소 업데이트

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:luciuschoi/rails7vue3.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 set :branch, 'main'
 set :stage, :production
-# set :rails_env, "production"
+set :rails_env, "production"
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "~/apps/#{fetch(:application)}"
