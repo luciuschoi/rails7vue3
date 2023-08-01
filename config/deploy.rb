38eb92e94620fc708cb2310f3ee13224075cf53b
Lucius Choi <lucius.choi@gmail.com>
2023-08-01 11:22:27 +0900

    rubygem.org -> https:// -> http://

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,16 +11,13 @@ set :stage, :production
 set :rails_env, "production"
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "~/apps/#{fetch(:application)}"
+set :deploy_to, "/home/deployer/apps/#{fetch(:application)}"
 
-set :rbenv_type, :user # or :system, or :fullstaq (for Fullstaq Ruby), depends on your rbenv setup
-# set :rbenv_ruby, '3.1.1'
 set :rbenv_path, '/home/deployer/.rbenv'
 set :rbenv_ruby, File.read('.ruby-version').strip
-
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
-set :rbenv_roles, :all # default value
+# set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+# set :rbenv_map_bins, %w{rake gem bundle ruby rails vite}
+# set :rbenv_roles, :all # default value
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -36,7 +33,7 @@ set :rbenv_roles, :all # default value
 # append :linked_files, "config/database.yml", 'config/master.key'
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor", "storage"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
